714b93b6e3bae143fcc34495f6d2376456e1265c
mikio kubo <mikiokubo@mikionoMacBook-Air.local>
2023-07-13 08:18:54 +0900

    prepared

--- analytics2/_modidx.py
+++ analytics2/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://scmopt.github.io',
                 'git_url': 'https://github.com/scmopt/analytics2',
                 'lib_path': 'analytics2'},
-  'syms': {'analytics2.core': {'analytics2.core.foo': ('core.html#foo', 'analytics2/core.py')}}}
+  'syms': {'analytics2.core': {'analytics2.core.foo': ('core.html#foo', 'analytics2/core.py')}, 'analytics2.networkx': {}}}
